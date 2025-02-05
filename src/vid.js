040b878f1f12a28227719c2ee5e04ac581969d0d
Sagar <sagar292905@gmail.com>
2025-02-05 16:56:45 +0530

    v1.0.1 - Fixed Bug: Background not showing

--- src/vid.js
+++ src/vid.js
@@ -35,16 +35,27 @@ function onPlayerReady() {
 }
 
 var background = [
-    "/lofi-radio/assets/img/Minecraft_Aesthetic_Photo.gif",
-    "/lofi-radio/assets/img/7heR4.gif",
-    "/lofi-radio/assets/img/Japan8BitGifs.gif",
-    "/lofi-radio/assets/img/Minecraft_Aesthetic.gif",
-    "/lofi-radio/assets/img/motel_pixel_art.gif",
-    "/lofi-radio/assets/img/pixel_art_cafe.gif",
-    "/lofi-radio/assets/img/pixel_art_gif_japan.gif",
-    "/lofi-radio/assets/img/retrowave_and_1980s_culture.gif",
-    "/lofi-radio/assets/img/roadgif.gif",
-    "/lofi-radio/assets/img/roomgif_rain.gif",
+  "https://i.pinimg.com/originals/ba/6d/5f/ba6d5f62d7bb1263a97b56631bd29e28.gif",
+  "https://i.pinimg.com/originals/4f/f0/88/4ff088795aff41e835671f1479fa0366.gif",
+  "https://i.pinimg.com/originals/86/c1/d9/86c1d9ace129663aa292a68fb889e80c.gif",
+  "https://i.pinimg.com/originals/43/1c/e5/431ce5d15aab86f66a91ec9a07c9114f.gif",
+  "https://i.pinimg.com/originals/13/7e/45/137e45c33cb5939abcd3ff4d8f858c63.gif",
+  "https://i.pinimg.com/originals/6d/78/71/6d78711d7c8438405ee8a5a50114f9ac.gif",
+  "https://i.pinimg.com/originals/1c/4a/45/1c4a4596912277e7b3b209f1ccf49927.gif",
+  "https://i.pinimg.com/originals/bf/fe/eb/bffeeb6e56305a13708c87d3df45a178.gif",
+  "https://i.pinimg.com/originals/53/0d/48/530d48aed30ade25448afa74d8a2959f.gif",
+  "https://i.pinimg.com/originals/bd/ec/4b/bdec4b1e34dc84a380e58f596766182d.gif",
+  "https://i.pinimg.com/originals/74/63/59/74635989b770a38189fff31a8ef152ea.gif",
+  "https://i.pinimg.com/originals/6f/d8/7b/6fd87b4c10f7538b5c530c53ad5ae844.gif",
+  "https://i.pinimg.com/originals/41/60/61/416061b9d95e206d7bbeb51e644cca6e.gif",
+  "https://i.pinimg.com/originals/ae/3f/99/ae3f993fd9ad5eb43319642e5e754e23.gif",
+  "https://i.pinimg.com/originals/6e/85/f7/6e85f7e0111ac569249afb790efff78f.gif",
+  "https://i.pinimg.com/originals/58/78/9f/58789f8e2d65c06f1218141cc8cd1db1.gif",
+  "https://i.pinimg.com/originals/c6/33/c2/c633c20ede82f0e0ced7d570dbe3a1f3.gif",
+  "https://i.pinimg.com/originals/40/01/14/400114aa763dcdff9aad8cf0d7194ba7.gif",
+  "https://i.pinimg.com/originals/26/45/47/2645475a9eef90f4a1fe67b76ae7d9fa.gif",
+  "https://i.pinimg.com/originals/79/d6/40/79d6400bad7992c811d56cb90d0beed2.gif",
+  "https://i.pinimg.com/originals/09/24/4c/09244c7f7dd4d17b0484370f32db6641.gif",
 ];
 
 const videoIds = [
@@ -79,7 +90,6 @@ document.getElementById("previous").addEventListener("click", () => {
 });
 
 function onPlayerStateChange(event) {
-    // console.log("Player state changed:", event.data, current);
     if (current == 1) {
         document.getElementById("static").innerText = "playing";
         document.getElementById("header").innerText = "(∪.∪ )...zzz";
